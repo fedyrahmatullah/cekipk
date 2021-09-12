87decf1bdcdbc9652b24dc46627e76dfcc18abd9
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2021-09-12 13:44:10 +0700

    Initial commit

---
+++ hasil.php
@@ -0,0 +1,163 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Inner Page - FlexStart Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: FlexStart - v1.4.0
+  * Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<?php include'header.php';?>
+<!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Hitung</li>
+        </ol>
+        <!-- <h2>Inner Page</h2> -->
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <section class="inner-page">
+      <div class="container">
+      <!-- Hitung   -->
+<?php
+	$baris = $_POST["jml_semester"];
+	$totalipk  = 0;
+	$nomor = 0;
+	$totalmutu = 0;
+	$totalsks = 0;
+
+	// $output = number_format($totalipk, 2, '.', '');
+	// algoritm
+	for ($i=0; $i < $baris; $i++) { 
+		$ipsemester[$i] = ($_POST["mutu"][$i]/$_POST["sks"][$i]);
+		$totalmutu += $_POST["mutu"][$i];
+		$totalsks += $_POST["sks"][$i];
+		$totalipk = $totalmutu/$totalsks;
+		
+	}
+ ?>
+<!DOCTYPE html>
+<html>
+<body>
+	<div class="clear"></div>
+	<div class="navbar">
+	<form action="index.php" method="post" id="jml_semester">
+		<label for="j_semester" class="label">Jumlah Semester: </label>
+		<input min="1" max="100" value="<?= $baris; ?>" type="number" id="j_semester" name="j_semester" required="" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+		<button type="submit" id="submit" name="submit">Submit</button>
+		<a href="index.php" class="btn-reset">Reset</a>
+	</form>
+	<div class="clear"></div>
+	</div>
+
+	<div class="container2">
+	<table>
+		<tr class="title centers">
+			<td rowspan="1">Semester Ke-</td>
+			<!-- <td rowspan="1">Semester ke-</td> -->
+			<td rowspan="1">Nilai Mutu</td>
+			<td colspan="1">SKS</td>
+			<td colspan="1">IP per Semester</td>
+		</tr>
+		<!-- <tr class="title centers">
+			<td>1 Hari</td>
+			<td><?= $_POST["pemakaian"]; ?> Hari</td>
+			<td>1 Hari</td>
+			<td><?= $_POST["pemakaian"]; ?> Hari</td>
+		</tr> -->
+	<?php for ($i=0; $i < $baris; $i++) : $nomor++;?>
+			<?php if ($nomor%2 == 0) :?>
+			<tr id="genap">
+			<?php else: ?>
+			<tr id="ganjil">
+			<?php endif ?>
+			<td class="centers"><?= $nomor; ?></td>
+			<!-- <td class="centers"><?= $_POST["nama"][$i]; ?></td> -->
+			<td class="centers"><?= $_POST["mutu"][$i]; ?></td>
+			<td class="centers"><?= $_POST["sks"][$i]; ?> sks</td>
+			<td class="centers"><?= number_format((float)$ipsemester[$i], 2, '.', ''); ?></td>
+		</tr>
+	<?php endfor ?>
+	<tr class="title">
+		<td colspan="7">
+			Total IPK <?= number_format((float)$totalipk, 2, '.', ''); ?><br>
+			<!-- Total IPK <?= $_POST["pemakaian"]; ?> Hari Rp. <?= $totalipk; ?>	 -->
+			</td>
+	</tr>
+	</table>
+	</div>
+	<div class="clear"></div>
+
+</body>
+</html>
+
+      <!-- End Hitung -->
+
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<?php include'footer.php';?>
+<!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ hitung.php
@@ -0,0 +1,238 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Inner Page - FlexStart Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: FlexStart - v1.4.0
+  * Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<?php include'header.php';?>
+<!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Hitung</li>
+        </ol>
+        <!-- <h2>Inner Page</h2> -->
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <section class="inner-page">
+      <div class="container">
+      <!-- Hitung   -->
+      <?php
+      $btnReset = false;
+$total = [
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","","",
+	"","","","","","","","","",""
+	];
+  $header = "
+
+  <meta charset=\"utf-8\">
+  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
+  <meta property=\"og:url\"                content=\"\" />
+  <meta property=\"og:type\"               content=\"article\" />
+  <meta property=\"og:title\"              content=\"Aplikasi perhitungan IPK\" />
+  <meta property=\"og:description\"        content=\"Aplikasi perhitungan IPK Mahasiswa Setiap Semester\" />
+  <meta property=\"og:image\"              content=\"assets\iconlogo-x.jpg\" />
+  
+  <link rel=\"manifest\" href=\"assets/icon/manifest.json\">
+  <meta name=\"msapplication-TileColor\" content=\"#ffffff\">
+  <meta name=\"msapplication-TileImage\" content=\"assets/icon/ms-icon-144x144.png\">
+  <meta name=\"theme-color\" content=\"#ffffff\">
+  <link rel=\"stylesheet\" type=\"text/css\" href=\"assets/css/style1.css\" />
+  ";
+
+    ?>
+<?php
+function validasiAngka(){
+	$input = $_POST["j_semester"];
+	$var = "/^[0-9]*$/";
+	if (!preg_match($var, $input)) {
+		echo "
+		<script>
+			alert('Data tidak sesuai ketentuan, Hanya menerima inputan angka');
+			document.location.href='index.php';
+		</script>
+		";
+	}elseif ($_POST["j_semester"] > 100 || $_POST["j_semester"] < 1 ) {
+		echo "
+		<script>
+			alert('Data melebihi kapasitas maksimal, Hanya menerima inputan 1 - 100');
+			document.location.href='index.php';
+		</script>
+		";		
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<body>
+	<div class="clear"></div>
+	<div class="clear"></div>
+	<div class="navbar">
+<!-- TEST COBA -->
+
+<div id="footer" class="footer">
+<div class="footer-newsletter" id="footer">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-12 text-center">
+        <h4>Our Newsletter</h4>
+        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+      </div>
+      <div class="col-lg-6">
+        <form action="" method="post">
+          <input type="email" name="email"><input type="submit" value="Subscribe">
+        </form>
+        <form action="" method="post" id="jml_semester">
+          <label for="j_semester" class="label2">Jumlah Semester: </label>
+          <?php if (isset($_POST["submit"])) : ?>
+          <?php $btnReset = true; ?>
+          <input min="1" max="100" value="<?= $_POST["j_semester"]; ?>" type="number" id="j_semester" name="j_semester" required="" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+          <?php else : ?>
+          <input min="1" max="100" placeholder="Masukan angka saja" type="number" id="j_semester" name="j_semester" required="" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+          <?php endif ?>
+          <button type="submit" id="submit" name="submit">Submit</button>
+          <?php if ($btnReset) {
+            echo "<a href=\"hitung.php\" class=\"btn-reset\">Reset</a>";
+          } ?>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+<!-- TES COBA END -->
+
+	<form action="" method="post" id="jml_semester">
+		<label for="j_semester" class="label2">Jumlah Semester: </label>
+		<?php if (isset($_POST["submit"])) : ?>
+		<?php $btnReset = true; ?>
+		<input min="1" max="100" value="<?= $_POST["j_semester"]; ?>" type="number" id="j_semester" name="j_semester" required="" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+		<?php else : ?>
+		<input min="1" max="100" placeholder="Masukan angka saja" type="number" id="j_semester" name="j_semester" required="" onkeypress="return event.charCode >= 48 && event.charCode <= 57">
+		<?php endif ?>
+		<button type="submit" id="submit" name="submit">Submit</button>
+		<?php if ($btnReset) {
+			echo "<a href=\"hitung.php\" class=\"btn-reset\">Reset</a>";
+		} ?>
+	</form>
+	<div class="clear"></div>
+	</div>
+	<?php if (isset($_POST["submit"])) : ?>
+		<?php validasiAngka(); ?>
+		<div class="container">
+		<form action="hasil.php" method="post">
+			<?php for ($i=1; $i<= $_POST["j_semester"]; $i++) :?>
+				<div class="inputan">
+					<h1>Semester <?= $i; ?></h1>
+					<!-- <div class="form-group">
+						<label for="nama<?= $i; ?>">Semester Ke-: </label><br>
+						<input type="text" id="nama<?= $i; ?>" name="nama[]" required="" placeholder="Semester ke-">
+					</div> -->
+					<div class="form-group">
+						<label for="sks<?= $i; ?>">Jumlah SKS: </label><br>
+						<input min="1" type="number" id="sks<?= $i; ?>" name="sks[]" required="" placeholder="Jumlah SKS Semester <?= $i; ?>">
+					</div>
+					<div class="form-group">
+						<label for="mutu<?= $i; ?>">Nilai Mutu per Semester: </label><br>
+						<input min="1" type="number_float" id="mutu<?= $i; ?>" name="mutu[]" required="" placeholder="Jumlah Nilai Mutu Semester <?= $i; ?>">
+					</div>
+				</div>
+			<?php endfor ?>
+		<div class="clear"></div>
+	</div>
+	<div class="clear"></div>
+	<div class="foother">
+
+			<input type="text" id="jml_semester" name="jml_semester" value="<?= $_POST["j_semester"]; ?>" hidden="hidden">
+			<button type="submit" id="enter" name="enter">Submit</button>
+		</form>
+		<div class="clear"></div>
+	</div>
+	<?php endif ?>
+		<div class="clear"></div>
+
+</body>
+</html>
+
+      <!-- End Hitung -->
+
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<?php include'footer.php';?>
+<!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>

a7462a2b03b62ad74c39ed04c1abb2bf7fa5c7b1
fedyrahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2024-08-13 23:53:50 +0700

    fix : correct path of route on vercel json

---
+++ api/hasil.php
@@ -0,0 +1,6 @@
+<?php
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../hasil.php';
+
+// api/index.php
+phpinfo();
---
+++ api/hitung.php
@@ -0,0 +1,6 @@
+<?php
+// Forward Vercel requests to normal index.php
+require __DIR__ . '/../hitung.php';
+
+// api/index.php
+phpinfo();

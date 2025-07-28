ad1df1be0e72ec01f7d8697f7896c68086a8a5aa
Fedy Rahmatullah <48233176+fedyrahmatullah@users.noreply.github.com>
2025-07-28 23:52:29 +0700

    feat: delete php info

--- api/hitung.php
+++ api/hitung.php
@@ -1,3 +1,6 @@
 <?php
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../hitung.php';
+
+// api/index.php
+phpinfo();
--- api/index.php
+++ api/index.php
@@ -1,6 +1,4 @@
 <?php
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../index.php';
-
-// api/index.php
-phpinfo();
+exit; // Pastikan phpinfo tidak dijalankan
